6c1e693b06e6f78d1019dac2fdbc187ba4f5fadc
AhcenChabbi <ma-chabbi@esi.dz>
2024-11-15 20:20:55 +0100

    faq section

---
+++ lib/constants.ts
@@ -0,0 +1,32 @@
+export type FaqItem = {
+  id: string;
+  question: string;
+  answer: string;
+};
+
+export const faqItems: FaqItem[] = [
+  {
+    id: "1",
+    question: "What is a conference?",
+    answer:
+      "A conference is a gathering of people for the purpose of discussing a topic or issue.",
+  },
+  {
+    id: "2",
+    question: "What is a conference?",
+    answer:
+      "A conference is a gathering of people for the purpose of discussing a topic or issue.",
+  },
+  {
+    id: "3",
+    question: "What is a conference?",
+    answer:
+      "A conference is a gathering of people for the purpose of discussing a topic or issue.",
+  },
+  {
+    id: "4",
+    question: "What is a conference?",
+    answer:
+      "A conference is a gathering of people for the purpose of discussing a topic or issue.",
+  },
+];
