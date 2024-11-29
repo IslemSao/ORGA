3d3ee4df3029f2a7573d79a2001324ba64720a41
Khadidja225 <mk_brakta@esi.dz>
2024-11-29 21:26:16 +0100

    done

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 function Navbar({}: Props) {
   return (
-    <div className=' text-white px-4 py-2 bg-slate-400 fixed'>
+    <div className=' text-white text-xl rounded-2xl font-bold flex justify-between items-center gap-16 px-16 py-3 shadow-custom-spread  bg-white backdrop-blur-md bg-opacity-10 fixed mt-28 '>
         <Link href="#">Home</Link>
         <Link href="#about">About</Link>
         <Link href="#about">Agenda</Link>
